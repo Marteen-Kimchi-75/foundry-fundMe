07709b8f1e5fe395e465f0c2bb17650828c07b0a
Kimchi <Marteen-Kimchi-75@users.noreply.github.com>
2025-01-29 13:06:31 +0000

    Added tests

--- test/unit/FundMeTest.t.sol
+++ test/unit/FundMeTest.t.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.24;
 
 import {Test, console} from "lib/forge-std/src/Test.sol";
-import {FundMe} from "../src/FundMe.sol";
-import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
